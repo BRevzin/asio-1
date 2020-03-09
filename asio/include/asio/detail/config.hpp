abe42b37863c27db8b4b86634f6c37694aa37103
Christopher Kohlhoff <chris@kohlhoff.com>
2020-03-09 22:23:11 +1100

    Support C++20 concept syntax.

--- asio/include/asio/detail/config.hpp
+++ asio/include/asio/detail/config.hpp
@@ -359,7 +359,11 @@
 # if !defined(ASIO_DISABLE_CONCEPTS)
 #  if __cpp_concepts
 #   define ASIO_HAS_CONCEPTS 1
-#   define ASIO_CONCEPT concept bool
+#   if __cpp_concepts >= 201707
+#    define ASIO_CONCEPT concept
+#   else // __cpp_concepts >= 201707
+#    define ASIO_CONCEPT concept bool
+#   endif // __cpp_concepts >= 201707
 #  endif // __cpp_concepts
 # endif // !defined(ASIO_DISABLE_CONCEPTS)
 #endif // !defined(ASIO_HAS_CONCEPTS)
